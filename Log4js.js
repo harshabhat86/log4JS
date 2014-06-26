96a4038a70e8b36fca821afe7da2ff03f13848a5
harshabhat86 <harshabhat@harshabhat-2.local>
2014-06-26 15:14:42 -0700

    The Logs are colorful and categorized
    The Logs are categorized into 4 types.
    1) log: The default… These are simple messages … dumb, plain and good
    old way… No colors, this is the default.
    
    2) Error: Errors… and marked in red
    
    3) warning: Orange
    
    4)message: Green

--- Log4js.js
+++ Log4js.js
@@ -1,8 +1,21 @@
 /*
- * @author mr.doob / http://mrdoob.com/
+ * 
+ @author Harsha Bhat @harshabhat86
+ Inspired by @mrdoob 's Logger.js.
+ Due credits to him. Here's the link to his repo incase you are interested.
+ https://github.com/mrdoob/logger.js
+ 
+ Just want to make it a little more useful.
+ 
+ Plans :
+ 1) Include different log formats and colors for them.
+ 2) Make log availability more easy and magical (dont know what that means)
+ 3) Try n make it async! (yes, in js)
+ 4) Option to save the log to a file (Export it real quick).
+ 
  */
 
-var Logger = function () {
+var Log4js = function () {
 
 	this.domElement = document.createElement( 'div' );
 	this.domElement.style.fontFamily = 'Helvetica, Arial, sans-serif';
@@ -27,6 +40,76 @@ var Logger = function () {
 		}
 
 	};
+    
+    /*
+    Error is marked in RED... Should it stop execution? No according to me, because I dont know if the user wants that
+    */
+    this.error = function ( msg, expand ) {
+        err = document.createElement('span');
+        err.style.color = 'RED';
+        err.appendChild(document.createTextNode( msg ));
+		this.domElement.appendChild( err );
+		this.domElement.appendChild( document.createElement( 'br' ) );
+
+		if ( expand && msg instanceof Object ) {
+
+			for ( var param in msg ) {
+                err.appendChild(document.createTextNode( '- ' + param + ': ' + msg[ param ] ) );
+                err.appendChild( document.createElement( 'br' ) );
+				
+			}
+            this.domElement.appendChild( err);
+
+		}
+
+	};
+    
+     /*
+    Warning is marked in Orange... 
+    */
+    this.warning = function ( msg, expand ) {
+        err = document.createElement('span');
+        err.style.color = 'rgb(239, 147, 24)';
+        err.appendChild(document.createTextNode( msg ));
+		this.domElement.appendChild( err );
+		this.domElement.appendChild( document.createElement( 'br' ) );
+
+		if ( expand && msg instanceof Object ) {
+
+			for ( var param in msg ) {
+                err.appendChild(document.createTextNode( '- ' + param + ': ' + msg[ param ] ) );
+                err.appendChild( document.createElement( 'br' ) );
+				
+			}
+            this.domElement.appendChild( err);
+
+		}
+
+	};
+    
+      /*
+    message is marked in green... 
+    */
+    this.message = function ( msg, expand ) {
+        err = document.createElement('span');
+        err.style.color = 'green';
+        err.appendChild(document.createTextNode( msg ));
+		this.domElement.appendChild( err );
+		this.domElement.appendChild( document.createElement( 'br' ) );
+
+		if ( expand && msg instanceof Object ) {
+
+			for ( var param in msg ) {
+                err.appendChild(document.createTextNode( '- ' + param + ': ' + msg[ param ] ) );
+                err.appendChild( document.createElement( 'br' ) );
+				
+			}
+            this.domElement.appendChild( err);
+
+		}
+
+	};
+    
 
 	this.clear = function () {
 
